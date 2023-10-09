2c44ec798fcef89993766f0b882205886fa51053
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-10-09 08:57:03 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+
+        T Get(Expression<Func<T, bool>> filter);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/Memory/InMemoryCarDal.cs
@@ -0,0 +1,76 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Memory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                 new Car { Id = 1, BrandId = 1, ColorId = 2, DailyPrice = 500, ModelYear = 2005, Description = "BMW 525" },
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(c => c.Id == car.Id);
+            _car.Remove(car);
+        }
+
+        public List<Car> Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetId(int carId)
+        {
+            return _car.Where(c => c.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+
+            Car carToUpdate = _car.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}  
+
+     
+
+       
+       
+
+     
+    
+
+     
+    
