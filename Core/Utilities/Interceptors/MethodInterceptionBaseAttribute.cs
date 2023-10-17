2a7549fa5e565bab9511bcf3631a5cbc00cc8343
Busra-nur-tosun <74898825+Busra-nur-tosun@users.noreply.github.com>
2023-10-17 10:43:54 +0300

    12. gün 3. ödev validaton aspect desteği aop

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Castle.DynamicProxy;
+
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }//önce hangisi çalışması gerekir ona göre
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+
+
+
+
+
+
+}
